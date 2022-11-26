c0d0dfac8b52ea6e6d1ac2966357f893a71969ea
FernandaAl2 <fernandaalves2021mogi@gmail.com>
2022-11-26 17:57:47 -0300

    first commit

---
+++ ex1.js
@@ -0,0 +1,29 @@
+//1 - Desenvolver um código que recebe um valor do usuário e verifique se é menor ou igual ou maior que 10 retornando a resposta na tela.
+
+ let valor; 
+
+ if(valor <10){
+
+
+
+    console.log("O valor é menor que 10");
+    else if(valor => 10){
+        console.log("Valor é maior que 10");
+
+    } 
+    else if (valor => 20){
+        console.log(Valor é maior que 20);
+   
+    }
+    else (valor  => 30){
+        console.logo(Valor é maior que 30)
+
+    }
+}
+
+    
+
+
+
+
+  
